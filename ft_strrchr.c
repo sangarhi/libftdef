400cc77e514054bd6be20b9f170e005fa8308466
sangarhi <sangarhi18@gmail.com>
2020-10-28 14:59:55 +0100

    jbn

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -17,11 +17,11 @@ char	*ft_strrchr(const char *str, int c)
 	int i;
 
 	i = ft_strlen(str);
-	while (i >= 0)
+	while (i > 0)
 	{
 		if (str[i] == c)
 			return ((char *)str + i);
 		i--;
 	}
-	return ((char *)str + i);
+	return (NULL);
 }
