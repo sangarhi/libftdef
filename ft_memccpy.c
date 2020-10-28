4a176aac6f79a649d7d07f59510c506ce8a039da
sangarhi <sangarhi18@gmail.com>
2020-10-28 15:33:06 +0100

    fwerf

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -23,6 +23,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	source =  (unsigned char *)src;
 	while (i < n)
 	{
+		dst[i] = source[i];
 		if (source[i] == (unsigned char)c)
 			return (dest + i + 1);
 		i++;
