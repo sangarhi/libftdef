7574fbb673cc717e8782dafa806d4e0fab5ccc50
Sandra Garcia hidalgo <sagarcia@c1r1s2.42madrid.com>
2020-10-28 22:06:53 +0100

    norme

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,32 +6,32 @@
 /*   By: sagarcia <sagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 19:38:47 by sagarcia          #+#    #+#             */
-/*   Updated: 2020/10/26 20:34:59 by sagarcia         ###   ########.fr       */
+/*   Updated: 2020/10/28 22:06:10 by sagarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    size_t  i;
+	size_t	i;
 
 	if (!dst || !src)
 		return (0);
-    i = 0;
-    while (src[i])
+	i = 0;
+	while (src[i])
 		i++;
-    if (dstsize == 0)
+	if (dstsize == 0)
 		return (i);
-    i = 0;
-    while (src[i] && i < dstsize - 1)
-    {
+	i = 0;
+	while (src[i] && i < dstsize - 1)
+	{
 		dst[i] = src[i];
 		i++;
-    }
-    dst[i] = '\0';
-    i = 0;
-    while (src[i])
+	}
+	dst[i] = '\0';
+	i = 0;
+	while (src[i])
 		i++;
 	return (i);
-} 
+}
