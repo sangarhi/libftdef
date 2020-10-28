68c007855689930799c58f06bc4f880ad95fe63e
Sandra Garcia hidalgo <sagarcia@c1r1s2.42madrid.com>
2020-10-28 18:30:10 +0100

    goeijg

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sagarcia <sagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 19:47:07 by sagarcia          #+#    #+#             */
-/*   Updated: 2020/10/26 20:23:12 by sagarcia         ###   ########.fr       */
+/*   Updated: 2020/10/28 18:25:15 by sagarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@ char	*ft_strrchr(const char *str, int c)
 	int i;
 
 	i = ft_strlen(str);
-	while (i > 0)
-	{
-		if (str[i] == c s[i] != c)
-			return ((char *)str + i);
+	while (i > 0 && str[i] != c)
 		i--;
-	}
+	if (str[i] == c)
+		return ((char *)str + i);
 	return (NULL);
+}
+
+int		main()
+{
+	char *src = "the cake is a lie !\0I'm hidden lol\r\n";
+	char *src2 = "the cake is a lie !\0I'm hidden lol\r\n";
+
+	printf( "s=%s\n", ft_strrchr(src, ' ') );
+	printf( "s=%s\n", strrchr(src2, ' ') );
+
+	return (0);
 }
