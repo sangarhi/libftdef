18c13588c01418c18b5cffc4f8c817358d7286f7
sangarhi <sangarhi18@gmail.com>
2020-10-28 22:00:37 +0100

    jdfj

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,21 +12,26 @@
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t i;
-	size_t src_len;
+    size_t  i;
 
-	i = 0;
-	src_len = ft_strlen(src);
-	if (dstsize != 0)
-	{
-		while (src[i] && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (src_len);
-}
+	if (!dst || !src)
+		return (0);
+    i = 0;
+    while (src[i])
+		i++;
+    if (dstsize == 0)
+		return (i);
+    i = 0;
+    while (src[i] && i < dstsize - 1)
+    {
+		dst[i] = src[i];
+		i++;
+    }
+    dst[i] = '\0';
+    i = 0;
+    while (src[i])
+		i++;
+	return (i);
+} 
