7b32cd46b0951a5586f3f90d51efdbbac39c3c9d
sangarhi <sangarhi18@gmail.com>
2020-10-30 02:52:18 +0100

    cjio

--- ft_split.c
+++ ft_split.c
@@ -6,95 +6,96 @@
 /*   By: sagarcia <sagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 19:29:12 by sagarcia          #+#    #+#             */
-/*   Updated: 2020/10/30 02:20:48 by sandra           ###   ########.fr       */
+/*   Updated: 2020/10/30 02:51:48 by sagarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		strs_len(char const *s, char c)
+static char			**ft_malloc_error(char **tab)
 {
-	int i;
-	int n;
+	unsigned int	i;
 
 	i = 0;
-	n = 1;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static unsigned int	ft_get_nb_strs(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	nb_strs;
+
+	if (!s[0])
+		return (0);
+	i = 0;
+	nb_strs = 0;
+	while (s[i] && s[i] == c)
+		i++;
 	while (s[i])
 	{
-		while (s[i] == c && s[i])
-			i++;
-		if (i > 0 && s[i] && s[i - 1] == c)
-			n++;
-		if (s[i])
-			i++;
-	}
-	if (i > 0 && n == 1 && s[i - 1] == c)
-		return (n);
-	if (s[0] != c)
-		n++;
-	return (n);
-}
-
-static int		s_len(char const *s, char c)
-{
-	int	i;
-	int	n;
-
-	i = 0;
-	n = 1;
-	while (s[i] != c && s[i])
-	{
-		i++;
-		n++;
-	}
-	return (n);
-}
-
-static int		fill_it(char **strs, char const *s, char c, int ls)
-{
-	int	i;
-	int	j;
-	int k;
-	int slen;
-
-	i = 0;
-	k = 0;
-	while (i < ls - 1)
-	{
-		j = 0;
-		while (s[k] == c && s[k])
-			k++;
-		slen = s_len(&s[k], c);
-		if (!(strs[i] = malloc(sizeof(char) * slen)))
-			return (0);
-		while (j + 1 < slen)
-			strs[i][j++] = s[k++];
-		strs[i][j] = '\0';
+		if (s[i] == c)
+		{
+			nb_strs++;
+			while (s[i] && s[i] == c)
+				i++;
+			continue ;
+		}
 		i++;
 	}
-	return (1);
+	if (s[i - 1] != c)
+		nb_strs++;
+	return (nb_strs);
 }
 
-char			**ft_split(char const *s, char c)
+static void			ft_get_next_str(char **next_str, unsigned int *next_str_len,
+					char c)
 {
-	int		ls;
-	char	**strs;
+	unsigned int i;
 
-	if (!s || !*s)
+	*next_str += *next_str_len;
+	*next_str_len = 0;
+	i = 0;
+	while (**next_str && **next_str == c)
+		(*next_str)++;
+	while ((*next_str)[i])
 	{
-		if (!(strs = malloc(sizeof(char *))))
-			return (NULL);
-		*strs = 0;
-		return (strs);
+		if ((*next_str)[i] == c)
+			return ;
+		(*next_str_len)++;
+		i++;
 	}
-	ls = strs_len(s, c);
-	if (!(strs = malloc(sizeof(char *) * ls)))
+}
+
+char				**ft_split(char const *s, char c)
+{
+	char			**tab;
+	char			*next_str;
+	unsigned int	next_str_len;
+	unsigned int	nb_strs;
+	unsigned int	i;
+
+	if (!s)
 		return (NULL);
-	if (!fill_it(strs, s, c, ls))
+	nb_strs = ft_get_nb_strs(s, c);
+	if (!(tab = (char **)malloc(sizeof(char *) * (nb_strs + 1))))
+		return (NULL);
+	i = 0;
+	next_str = (char *)s;
+	next_str_len = 0;
+	while (i < nb_strs)
 	{
-		free(strs);
-		return (NULL);
+		ft_get_next_str(&next_str, &next_str_len, c);
+		if (!(tab[i] = (char *)malloc(sizeof(char) * (next_str_len + 1))))
+			return (ft_malloc_error(tab));
+		ft_strlcpy(tab[i], next_str, next_str_len + 1);
+		i++;
 	}
-	strs[ls - 1] = 0;
-	return (strs);
+	tab[i] = NULL;
+	return (tab);
 }
